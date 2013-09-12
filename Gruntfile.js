3447e22ab8b9747b8a27c89c4ecf05f1f4a526f3
Danny Douglass <ddouglass@gmail.com>
2013-09-12 18:15:12 -0400

    re-adding Procfile with proper casing

--- Gruntfile.js
+++ Gruntfile.js
@@ -298,7 +298,7 @@ module.exports = function (grunt) {
                     dest: '<%= yeoman.dist %>',
                     src: [
                         'package.json',
-                        'procfile'
+                        'Procfile'
                     ]
                 }]
             },
